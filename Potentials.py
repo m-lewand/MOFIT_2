d44e4842598a33f2521df6b59b15cecda5b74827
Julian Czarnecki <jczarnecki86@gmail.com>
2022-11-22 11:08:19 +0100

    up

--- Potentials.py
+++ Potentials.py
@@ -9,6 +9,6 @@ def Parabolic(element, i, j, m, omega, a, N):
     for l in range(0,3):
         for n in range(0,3):
             v += a**2/4.*m*omega**2/2. * w_gauss[l]*w_gauss[n]* MES.shape_function(p_gauss[l],p_gauss[n],j)* \
-            MES.shape_function(p_gauss[l],p_gauss[n],i)*(Mesh.find_real_space_coordinate(Mesh.find_global_number(element, i,N), N,a)[0]**2 \
-                + Mesh.find_real_space_coordinate(Mesh.find_global_number(element, j,N), N,a)[1]**2)
+            MES.shape_function(p_gauss[l],p_gauss[n],i)*((Mesh.find_real_space_coordinate(Mesh.find_global_number(element, 1,N), N,a)[0]/2 * (1-p_gauss[l]) + Mesh.find_real_space_coordinate(Mesh.find_global_number(element, 2,N), N,a)[0]/2 * (1+p_gauss[l]) )**2 \
+                + (Mesh.find_real_space_coordinate(Mesh.find_global_number(element, 1,N), N,a)[1]/2 * (1-p_gauss[n]) + Mesh.find_real_space_coordinate(Mesh.find_global_number(element, 2,N), N,a)[1]/2 * (1+p_gauss[n]) )**2)
     return v 
